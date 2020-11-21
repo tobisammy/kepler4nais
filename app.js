b2356d9b39bd7ff6ff3a0c70318b5978072d4409
tobisammy <tobisamuel0173@gmail.com>
2020-11-21 14:40:35 -0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        document.getElementById('splash').classList.toggle('fade');
-        setTimeout(() =>{
-            document.getElementById('splash').style.display = 'none';
-        },3000)
-    }, 2000);
-    
-})
